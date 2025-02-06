74445a91168a390c10e8e4952caf02da0206514d
Lox-01 <128305675+Lox-01@users.noreply.github.com>
2025-02-06 08:05:26 -0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
+from flask import Flask, render_template, request, redirect, url_for
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
+# After running this code without errors, you may navigate to http://localhost:8080 or http://127.0.0.1:8080, to see index.html rendered.
