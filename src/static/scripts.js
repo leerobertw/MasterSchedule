8625887bf5cce4e237b0baf00e1963d6381bbe53
AWorthington55 <worthash2022@gmail.com>
2025-02-11 13:43:34 -0700

    Some major changes to index and scripts. The user can now input up to 10 classes that they want to take during the 3 years they're in lone peak.

--- src/static/scripts.js
+++ src/static/scripts.js
@@ -2,32 +2,26 @@
 
 //getting the information from the form
 let form = document.querySelector('#form');
-let A1 = document.querySelector('#A1');
-let A2 = document.querySelector('#A2');
-let A3 = document.querySelector('#A3');
-let A4 = document.querySelector('#A4');
-let B5 = document.querySelector('#A5');
-let B6 = document.querySelector('#B6');
-let B7 = document.querySelector('#B7');
-let B8 = document.querySelector('#B8');
-let other  = document.querySelector('#other');
+let class1 = document.querySelector('#class1');
+let class2 = document.querySelector('#class2');
+let class3 = document.querySelector("#class3");
+let class4 = document.querySelector("#class4");
+let class5 = document.querySelector("#class5");
+let class6 = document.querySelector("#class6");
+let class7 = document.querySelector("#class7");
+let class8 = document.querySelector("#class8");
+let class9 = document.querySelector("#class9");
+let class10 = document.querySelector("#class10");
+
+
 
 //class information
-let schedule = document.querySelector("#schedule");
+let information = document.querySelector("#information");
 let class_name = document.querySelector("#class_name");
 let teachers = document.querySelector("#teachers");
 let class_periods = document.querySelector("#class_periods");
 let categories = document.querySelector("#categories");
 let prerequisites = document.querySelector("#prerequisites");
 
-//making the schedule
-let first_period = document.querySelector("#first");
-let second_period = document.querySelector("#second");
-let third_period = document.querySelector("#third");
-let fourth_period = document.querySelector("#fourth");
-let fifth_period = document.querySelector("#fifth");
-let sixth_period = document.querySelector("#sixth");
-let seventh_period = document.querySelector("#seventh");
-let eighth_period = document.querySelector("#eighth");
-let other_classes = document.querySelector("#other_classes");
+
 
