a79e1917caa0e53cf64cd67ffa1ef92180ab97c7
AWorthington55 <worthash2022@gmail.com>
2025-03-17 13:41:20 -0600

    Attempting to get the js to work, but since pycharm is making me use node.js, it isn't really working right now. I made a minor change, but I'm still trying to figure out how node.js works

---
+++ src/static/scripts.cjs
@@ -0,0 +1,77 @@
+import {classes} from './schedule_complete.js';
+classes();
+
+const submitBtn = document.querySelector('#submit');
+submitBtn.addEventListener("click", () => {
+    let classOne = document.querySelector('input[name="class1"]');
+    localStorage.setItem('first-class', classOne.value);
+    let classTwo = document.querySelector('input[name="class2"]');
+    localStorage.setItem('second-class', classTwo.value);
+    let classThree = document.querySelector('input[name="class3"]');
+    localStorage.setItem('third-class', classThree.value);
+    let classFour = document.querySelector('input[name="class4"]');
+    localStorage.setItem('fourth-class', classFour.value);
+    let classFive = document.querySelector('input[name="class5"]');
+    localStorage.setItem('fifth-class', classFive.value);
+    let classSix = document.querySelector('input[name="class6"]');
+    localStorage.setItem('sixth-class', classSix.value);
+    let classSeven = document.querySelector('input[name="class7"]');
+    localStorage.setItem('seventh-class', classSeven.value);
+    let classEight = document.querySelector('input[name="class8"]');
+    localStorage.setItem('eighth-class', classEight.value);
+    let classNine = document.querySelector('input[name="class9"]');
+    localStorage.setItem('ninth-class', classNine.value);
+    let classTen = document.querySelector('input[name="class10"]');
+    localStorage.setItem('tenth-class', classTen.value);
+})
+
+const resetBtn = document.querySelector("#reset");
+resetBtn.addEventListener('click', () => {
+    localStorage.removeItem('class1');
+    localStorage.removeItem('class2');
+    localStorage.removeItem('class3');
+    localStorage.removeItem('class4');
+    localStorage.removeItem('class5');
+    localStorage.removeItem('class6');
+    localStorage.removeItem('class7');
+    localStorage.removeItem('class8');
+    localStorage.removeItem('class9');
+    localStorage.removeItem('class10');
+    location.reload();
+})
+
+const check = localStorage.getItem('class1');
+if (check === null) {
+    document.querySelector('#form').className = 'show';
+    document.querySelector('#class_information').className = 'hide';
+} else {
+    document.querySelector('#form').className = 'hide';
+    document.querySelector('#class_information').className = 'show'
+}
+
+const classInfo = document.querySelector('#class_information');
+
+for (let x = 0; x < classes.length; x++) {
+    let information = document.createElement('div');
+    let classText = document.createElement('h4');
+    classText.innerHTML = `Class name: ${classes[x].class_name}`;
+    let teacherText = document.createElement('h4');
+    teacherText.innerHTML = `Teacher(s): ${classes[x].teacher(s)}`;
+    let roomText = document.createElement('h4');
+    roomText.innerHTMl = `Room: ${classes[x].room}`;
+    let periodText = document.createElement('h4');
+    periodText.innerHTML = `Periods: ${classes[x].periods}`;
+    let semesterText = document.createElement('h4');
+    semesterText.innerHTML = `Semesters: ${classes[x].semesters}`;
+    let figure = document.createElement('figure');
+    figure.appendChild(classText);
+    figure.appendChild(teacherText);
+    figure.appendChild(roomText);
+    figure.appendChild(periodText);
+    figure.appendChild(semesterText)
+    classInfo.appendChild(figure);
+}
+
+
+
+
