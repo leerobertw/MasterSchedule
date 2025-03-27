9e27b0600323fbcf6617b4f400273bc30c96a769
Lox-01 <128305675+Lox-01@users.noreply.github.com>
2025-03-27 08:43:23 -0600

    If the schedule is valid, shows the combinations. If the schedule is not valid, shows issue classes.

--- README.md
+++ README.md
@@ -2,3 +2,28 @@
 School course master schedule organizer and validator
 
 FILES BEGINNING WITH F ARE FLASK EXAMPLES. THEY WERE RENAMED TO AVOID CONFLICT WITH OTHER FILES.
+
+For the "serving" branch and its functionality, I use the following class combination to test a working schedule:
+
+1. SPANISH 3* - Concurrent Enrollment
+2. MATH 1010-Concurrent Enrollment
+3. CALCULUS AB LAB
+4. SPANISH 4* - Concurrent Enrollment
+5. AP SPANISH LANGUAGE*
+6. ART HONORS*
+7. PAINTING 2
+8. DRAWING 1
+
+The order of classes entered does not matter.
+For the "serving" branch and its functionality, I use the following class combination to test a non-working schedule:
+
+1. RESOURCE ENGLISH*
+2. MATH 1010-Concurrent Enrollment
+3. CALCULUS AB LAB
+4. SPANISH 4* - Concurrent Enrollment
+5. AP SPANISH LANGUAGE*
+6. ART HONORS*
+7. PAINTING 2
+8. DRAWING 1
+
+Resource English and AP Spanish Language are both only available during period 3.
--- src/fmain.py
+++ src/fmain.py
@@ -13,6 +13,15 @@ app.jinja_env.filters["fromjson"] = json.loads
 def index():
     return render_template('index.html')
 
+def get_valid(classes):
+    all_combinations = L()
+    for class_name, periods in classes.items():
+        if class_name and periods:
+            all_combinations.p(periods)
+    all_combinations.c()
+    valid_combinations = [comb for comb in all_combinations if len(set(comb)) == len(comb)]
+    return valid_combinations, len(valid_combinations)>0
+
 @app.route('/check_schedule', methods=['POST'])
 def check_schedule():
     form_data = request.form.to_dict()
@@ -20,39 +29,32 @@ def check_schedule():
     classes = {form_data[f'class{i}']: [] for i in range(1, 9)}
     for classs, _ in classes.items():
         classes[classs] = list(set([classss[1] for classss in schedule if classss[0] == classs]))
-    all_combinations = L()
-    for class_name, periods in classes.items():
-        if class_name and periods:
-            all_combinations.p(periods)
-    all_combinations.c()
-    valid_combinations = [comb for comb in all_combinations if len(set(comb)) == len(comb)]
-    mapped_class_combinations = [dict(sorted({valid_combination[i]:classnames[i] for i in range(len(classnames))}.items())) for valid_combination in valid_combinations]
-    if len(valid_combinations) <= 0:
+    valid_combinations, valid = get_valid(classes)
+    if not valid:
         period_conflicts = {}
-        valid_periods = {class_name: set() for class_name in classes}
-        for valid_combination in all_combinations:
-            for i, class_name in enumerate(classnames):
-                valid_periods[class_name].add(valid_combination[i])
-        conflicts = {period: [class_name for class_name, periods in valid_periods.items() if period in periods] for period in [item for item, count in Counter([item for value in valid_periods.values() for item in value]).items() if count > 1]}
-        print(conflicts)
-        """
         for class_name, periods in classes.items():
-            conflicting_periods = [p for p in periods if p not in valid_periods[class_name]]
-            print(f"Class {class_name} has conflicting periods:", conflicting_periods)
-            for period in conflicting_periods:
+            for period in periods:
                 if period in period_conflicts:
                     period_conflicts[period].append(class_name)
                 else:
                     period_conflicts[period] = [class_name]
-        print("Final period conflicts:", period_conflicts)
-        conflicting_classes = {p: c for p, c in period_conflicts.items() if len(c) > 1}
-        print("Filtered conflicting classes (only periods with multiple conflicts):", conflicting_classes)
-        msg = {"status": "You can't take those classes next year!", "conflicts": conflicting_classes}
-        flash(json.dumps(msg))"
-        """
+        conflicting_classes = {k: v for k, v in sorted(period_conflicts.items()) if len(v) > 1}
+        removable_classes = []
+        for class_name in list(classes.keys()):
+            temp_classes = {k: v for k, v in classes.items() if k != class_name}
+            _, temp_valid = get_valid(temp_classes)
+            if temp_valid:
+                removable_classes.append(class_name)
+        msg = {
+            "status": "You can't take those classes next year!",
+            "conflicts": removable_classes
+        }
+        flash(json.dumps(msg))
     else:
-        msg = "You can take those classes next year!"
-        flash(json.dumps({"status":msg,"result":mapped_class_combinations}))
+        mapped_class_combinations = [dict(sorted({valid_combination[i]: classnames[i] for i in range(len(classnames))}.items())) for valid_combination in valid_combinations]
+        msg = {"status": "You can take those classes next year!", "result": mapped_class_combinations}
+        flash(json.dumps(msg))
+    
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
