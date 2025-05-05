4b8fb6402e6e1a72af93edad4d2c101a17f6684b
Robert Lee <robert.leefam@gmail.com>
2025-05-05 13:18:39 -0600

    Added 2025-26 csv files. Started tool to convert csv to json format.

---
+++ src/tools/test_CSV_to_JSON.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+from CSV_to_JSON import *
+
+class Test(TestCase):
+    def test_build_courses(self):
+        department = Department("English")
+        teacher = "CARPENTER"
+        room = 614
+        data = 'ENGLISH 10*,PREP,ACCELERATED READING,ENGLISH 10*,ENGLISH 10*,PREP,ENGLISH 10*,JOURNALISM*'.split(',')
+        semester = 1
+        classes = build_courses(department, teacher, room, 1, data)
+        self.assertEqual(len(classes), 8)
+        class1 = classes[0]
+        self.assertEqual(class1.name, 'ENGLISH 10*')
+        self.assertEqual(class1.teacher, teacher)
+        self.assertEqual(class1.room, room)
+        self.assertEqual(class1.department, department)
+        self.assertEqual(class1.period, 'A1')
